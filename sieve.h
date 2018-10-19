1c94f716df31c5c5299cc33cec5892db8cbf1080
huiminthehuman <huiminwu24@gmail.com>
2018-10-19 15:36:33 -0400

    Update classwork

---
+++ sieve.h
@@ -0,0 +1,1 @@
+void sieve(int n);
--- test.c
+++ test.c
@@ -3,25 +3,30 @@
 #include "sieve.h"
 
 //first param is the number, second param is the num of iterations
-int main(int argc, char * argv[]){
-  int iterations = 1;
-  int target = 1000000;
-  if(argc > 1){
-    target = atoi(argv[1]);
-  }
-  if(argc > 2){
-    iterations = atoi(argv[2]);
-  }
-
-
-  int ans = 0;
-  while(iterations>0){
-      ans=sieve(target);
-      iterations--;
-      printf("The n=%d prime is %d\n", target, ans );
-      //this is to modify which prime to
-      //potentially avoid CPU caching
-      target++;
-  }
-  return 0;
+//int main(int argc, char * argv[]){
+int main() {
+    sieve(18);
+    return 0;
 }
+//  int iterations = 1;
+//  int target = 1000000;
+//  if(argc > 1){
+//    target = atoi(argv[1]);
+//  }
+//  if(argc > 2){
+//    iterations = atoi(argv[2]);
+//  }
+//
+//
+//  int ans = 0;
+ // while(iterations>0){
+ //     ans=sieve(target);
+ //     iterations--;
+ //     printf("The n=%d prime is %d\n", target, ans );
+ //     //this is to modify which prime to
+ //     //potentially avoid CPU caching
+ //     target++;
+ // }
+//  sieve(target);
+//  return 0;
+//}
